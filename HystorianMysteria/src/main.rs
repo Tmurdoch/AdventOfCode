7d40f56c0d122fc78874f804643010da12c8a9c4
Thomas <thomasjmurd@gmail.com>
2024-12-01 22:55:28 -0600

    trying to get file input to work, see: https://stackoverflow.com/questions/61469078/i-keep-getting-parseinterror-kind-invaliddigit-in-my-rust-program-and-i-hav check what is the delimeter for input file

--- HystorianMysteria/src/main.rs
+++ HystorianMysteria/src/main.rs
@@ -1,10 +1,20 @@
+use std::fs;
+
 fn main() {
     use std::io::{stdin, stdout, Write};
-    let mut s=String::new();
-    let _=stdout().flush();
-    stdin().read_line(&mut s).expect("Not a string");
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
+
+    //let mut file = std::fs::File::open("input.txt").unwrap();
+    let contents = fs::read_to_string("input1.txt").expect("could not read file");
+    //file.read_to_string(&mut contents).unwrap();
+
+    
+    let mut input: Vec<i32> = Vec::new();
+    for s in contents.split("   ") {
+        input.push(s.trim().parse::<i32>().unwrap());
     }
-    println!("Got: {}", s);
+    let _=stdout().flush();
+    //if let Some('\n')=s.chars().next_back() {
+    //    s.pop();
+    //}
+    println!("Got: {:?}", input);
 }
