bc5ed5a0463acc508d71bc0319553617ed18dc53
Thomas <thomasjmurd@gmail.com>
2024-12-02 22:42:39 -0600

    got some lists, need to subtract them, see: https://users.rust-lang.org/t/how-to-iterate-through-two-arrays-at-once/783/2~ maybe

--- HystorianMysteria/src/main.rs
+++ HystorianMysteria/src/main.rs
@@ -3,13 +3,30 @@ use std::fs;
 fn main() {
     use std::io::{stdin, stdout, Write};
     let contents = fs::read_to_string("input1.txt").expect("could not read file");
-    let mut input: Vec<i32> = Vec::new();
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    let mut count = 0;
     for s in contents.split_whitespace() {
-        input.push(s.trim().parse::<i32>().unwrap());
+        if count % 2 == 0{ 
+            left.push(s.trim().parse::<i32>().unwrap());
+        }
+        else {
+            right.push(s.trim().parse::<i32>().unwrap());
+        }
+        count+=1;
     }
     let _=stdout().flush();
-    //if let Some('\n')=s.chars().next_back() {
-    //    s.pop();
-    //}
-    println!("Got: {:?}", input);
+    left.sort();
+    right.sort();
+    let mut output = 0;
+    for i in &left {
+        for j in &right {
+            let n = i - j;
+            output += n.abs();
+        }
+    }
+
+    println!("Got sum: {:?}", output);
 }
