5839596226e9c15c1d04088a6db2805c3ccbe932
Thomas <thomasjmurd@gmail.com>
2024-12-02 22:08:44 -0600

    numbers in list

--- HystorianMysteria/src/main.rs
+++ HystorianMysteria/src/main.rs
@@ -2,14 +2,9 @@ use std::fs;
 
 fn main() {
     use std::io::{stdin, stdout, Write};
-
-    //let mut file = std::fs::File::open("input.txt").unwrap();
     let contents = fs::read_to_string("input1.txt").expect("could not read file");
-    //file.read_to_string(&mut contents).unwrap();
-
-    
     let mut input: Vec<i32> = Vec::new();
-    for s in contents.split("   ") {
+    for s in contents.split_whitespace() {
         input.push(s.trim().parse::<i32>().unwrap());
     }
     let _=stdout().flush();
--- HystorianMysteria/target/debug/HystorianMysteria.d
+++ HystorianMysteria/target/debug/HystorianMysteria.d
@@ -1,1 +1,1 @@
-C:\Users\kewl2\Projects\AdventOfCode\HystorianMysteria\target\debug\HystorianMysteria.exe: C:\Users\kewl2\Projects\AdventOfCode\HystorianMysteria\src\main.rs
+/home/thomas/Projects/AdventOfCode/HystorianMysteria/target/debug/HystorianMysteria: /home/thomas/Projects/AdventOfCode/HystorianMysteria/src/main.rs
