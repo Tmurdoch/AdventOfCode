7dd338c7769efef0a4827ddacbf0a0c5eac44e53
Thomas <thomasjmurd@gmail.com>
2024-12-17 20:18:37 -0600

    finished part 2 of day 1

--- HystorianMysteria/src/main.rs
+++ HystorianMysteria/src/main.rs
@@ -1,7 +1,21 @@
 use std::fs;
 
+/*
+ * counts how many times num appears in nums
+ * multiplied count by number and returns
+ */
+fn counts(mut nums: Vec<i32>, num: i32) -> i32 {
+    let mut count = 0;
+    for i in nums.iter_mut() {
+        if *i == num {
+            count+=1;
+        }
+    }
+    return count * num;
+}
+
 fn main() {
-    use std::io::{stdin, stdout, Write};
+    use std::io::{stdout, Write};
     let contents = fs::read_to_string("input1.txt").expect("could not read file");
 
     let mut left: Vec<i32> = Vec::new();
@@ -27,12 +41,15 @@ fn main() {
     //        output += n.abs();
     //    }
     //}
-    for it in left.iter().zip(right.iter_mut()) {
-        let (ai, bi) = it;
-        let n = *ai - *bi;
-        println!("diff: {:?}", n);
-
-        output += n.abs();
+    //for it in left.iter().zip(right.iter_mut()) {
+    //    let (ai, bi) = it;
+    //    let n = *ai - *bi;
+    //    println!("diff: {:?}", n);
+    //
+    //    output += n.abs();
+    //}
+    for i in left.iter() {
+        output += counts(right.clone(), *i);
     }
 
     println!("Got sum: {:?}", output);
