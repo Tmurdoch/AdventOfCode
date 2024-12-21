c705584094d46d6a06cae663af99e89e0f3d6803
Thomas <thomasjmurd@gmail.com>
2024-12-21 17:21:13 -0600

    hooked up tester function, started parse the ints, see link

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,34 +1,56 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 use std::option;
 
-
 fn parse_with_regex(line: &str) -> option::Option<Vec<&str>> {
-
     let pattern = r"mul\(\d+,\d+\)";
     let re = Regex::new(pattern).unwrap();
     let mut return_list: Vec<&str> = Vec::new();
     for matching_str in re.find_iter(line) {
         return_list.push(matching_str.as_str())
     }
-    return Some(return_list); 
+    return Some(return_list);
 }
 
-fn extract_tuple_from_str(value: &str) -> (i32, i32) {
+//see https://doc.rust-lang.org/book/ch04-03-slices.html
+fn extract_tuple_from_str(value: &str) -> Option<(i32, i32)> {
+    let trimmed_value = value.trim();
+    //let first_val: i32;
+    //let second_val: i32;
 
+    let parts: Vec<&str> = value.split(',').filter(|s| !s.trim().is_empty()).collect();
+    if parts.len() == 2 {
+        let first = parts[0].trim().parse::<i32>().ok();
+        let second = parts[1].trim().parse::<i32>().ok();
+        return Some((first, second));
+    } else {
+        return None;
+    }
+    //let bytes = trimmed_value.as_bytes();
+    //for (_i, &item) in bytes.iter().enumerate() {
+    //    if item.is_ascii_digit() {
+    //        println!("found digit: {:?}", item);
+    //    }
+    //
+    //    //get sliced with &value[..]
+    //}
+
+    //return (first_val, second_val);
 }
 
 fn test_extraction(value: &str, expected: (i32, i32)) {
     let val: (i32, i32) = extract_tuple_from_str(value);
     if val != expected {
-         panic!("expected: {:?}, got {:?} ", expected, val);
+        panic!("expected: {:?}, got {:?} ", expected, val);
     }
 }
- 
+
 fn main() {
     let contents = fs::read_to_string("src/day3input.txt").expect("could not read file");
 
-    let mut all_commands: Vec<&str> = Vec::new(); 
+    test_extraction("0 0", (0, 0));
+
+    let mut all_commands: Vec<&str> = Vec::new();
     for line in contents.lines() {
         let found_commands = parse_with_regex(line);
 
@@ -36,10 +58,4 @@ fn main() {
             all_commands.push(&val);
         }
     }
-
-    for command in all_commands {
-        let starting_num_index = command.find(|c: char| c >= '0' && c <= '9');
-        
-    }
 }
-
