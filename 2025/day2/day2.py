4705d6b417980cca95590124ae940ab4f475103a
Thomas <thomasjmurd@gmail.com>
2026-02-10 15:06:25 -0600

    day2 part 1 done

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -11,5 +11,21 @@ for r in ranges:
     both = r.split("-")
     formatted_ranges.append([int(x) for x in both])
 
+invalids = []
 
-print(formatted_ranges)
+for f in formatted_ranges:
+    first, second = f
+    for i in range(first, second + 1):
+        string_number = str(i)
+        if len(string_number) % 2 != 0:
+            continue
+        string1 = string_number[0:int(len(string_number) / 2)]
+        string2 = string_number[int(len(string_number) / 2) : ]
+        if int(string1) == int(string2):
+            #match
+            invalids.append(int(string_number))
+
+print(sum(invalids))
+
+
+
